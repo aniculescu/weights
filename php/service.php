d253b2606f3bf2b8c01d71a3980dc9f58624122f
aniculescu <andrew@aniculescu.com>
2014-03-07 11:01:43 -0800

    Initial commit

---
+++ php/service.php
@@ -0,0 +1,73 @@
+<?php
+require_once('dbConnect.php');
+
+class Weights
+{
+    private $currentDate;
+    private $mydb;
+    public function __construct(){
+        $this->currentDate = date("Y-m-d");
+        $this->mydb = new Database($__dbConfig["hostname"], $__dbConfig["username"], $__dbConfig["password"], $__dbConfig["dbName"]);
+    }
+
+    public function addExercise(){
+        // Add a new exercise to the list of exercises
+    }
+
+    private function addWeight($user_id, $exercise, $weight, $date){
+        $this->mydb->connect();
+        // Check to see if date/weight combo already exists
+        $checkQuery = "SELECT id FROM schedule WHERE date = '{$date}' AND exercise_id = {$exercise} AND user_id = {$user_id} LIMIT 1";
+        $checkResult = $this->mydb->query($checkQuery);
+        $checkRows = mysql_fetch_row($checkResult);
+                
+        if(!$checkRows){
+            // If data does not exist, add new row
+            $insertQuery = "INSERT INTO schedule (date, user_id, exercise_id, weight) VALUES ('{$date}',{$user_id},{$exercise},{$weight})";
+            $this->mydb->query($insertQuery);
+        } else {
+            // If data exists, update
+            $updateQuery = "UPDATE schedule SET weight = {$weight} WHERE id = {$checkRows[0]} LIMIT 1";
+            $this->mydb->query($updateQuery);
+        }
+        $this->mydb->close();
+    }
+    
+    private function showLastWeight($exercise){
+        // Show the weight for the last time the exercise was performed
+    }
+    
+    public function getAllPrevWeights($user_id){
+        $this->mydb->connect();
+        // Grab a list of all exercises
+        $allExercisesQuery = "SELECT * FROM exercises ORDER BY id ASC";
+        $allExercises = $this->mydb->fetch_all_array($allExercisesQuery);
+        // Find the weight for the last time the specified user performed the exercise
+        foreach($allExercises as $key => $exercise){
+            $userExerciseQuery = "SELECT * FROM  schedule WHERE user_id = {$user_id} AND exercise_id = {$exercise['id']} ORDER BY date DESC LIMIT 1";
+            $userExercise = $this->mydb->fetch_all_array($userExerciseQuery);
+            if(count($userExercise) > 0){
+                $allExercises[$key]['userData'] = $userExercise[0];
+            }
+        }
+        $this->mydb->close();
+        return $allExercises;
+    }
+    
+    public function addWorkout($data){
+        // Add a whole day of exercises all at once from a JSON object which includes user and array of exercises
+        $date = $data['date'];
+        $user_id = $data['user_id'];
+        
+        unset($data['date']);
+        unset($data['user_id']);
+                
+        foreach($data as $exid => $weight){
+            if($weight > 0){
+                $this->addWeight($user_id, $exid, $weight, $date);
+            }
+        }
+    }
+}
+
+?>
